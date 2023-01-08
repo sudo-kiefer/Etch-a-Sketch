bd7c9940deeae224bb6009d0ef37c72752d8ce89
sudo-kiefer <73907782+sudo-kiefer@users.noreply.github.com>
2023-01-08 08:34:39 -0800

    Add style selectors to update grid row and column count

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const sizeSelector = 16;
+const sizeSelector = 33;
+
+function randomColor1() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var Color1 = "rgb(" + x + "," + y + "," + z + ")";
+    return Color1;
+    };
+
+function randomColor2() {
+    var z = Math.floor(Math.random() * 256);
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var Color2 = "rgb(" + x + "," + y + "," + z + ")";
+    return Color2;
+    };
 
 function loadDivs() {
 
@@ -8,21 +24,21 @@ function loadDivs() {
         newContent.id = 'div' + i;
         const divTarget = document.getElementById("container");
         divTarget.appendChild(newContent);
-        console.log("Element created.");
-        
-        //Change color on mouse-in
+
+        //Randomize color on mouse-in
         const gridSelector = document.getElementById("div" + i);
         gridSelector.addEventListener("mouseover", function() {
-            gridSelector.style.backgroundColor = "yellow";
+            gridSelector.style.backgroundColor = randomColor1();
         });
-        //Change color on mouse-out
+        //Randomize color on mouse-out
         gridSelector.addEventListener("mouseout", function() {
-            gridSelector.style.backgroundColor = "red";
+            gridSelector.style.backgroundColor = randomColor2();
         });
     
         
     }
-
-
+    //Modify grid columns count based on selected size
+    document.getElementById("container").style.gridTemplateColumns = "repeat(" + sizeSelector + ", " + 1 + "fr)";
+    document.getElementById("container").style.gridTemplateRows = "repeat(" + sizeSelector + ", " + 1 + "fr)";
 };
 
