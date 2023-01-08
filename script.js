83a10c4ebc972c99d4fd6b17ee81e8f92be12d64
sudo-kiefer <73907782+sudo-kiefer@users.noreply.github.com>
2023-01-08 07:39:44 -0800

    Add mouse-in mouse-out event listeners with color change events

--- script.js
+++ script.js
@@ -2,18 +2,27 @@ const sizeSelector = 16;
 
 function loadDivs() {
 
-    console.log("function initiated.");
-
     for(let i = 0; i < (sizeSelector*sizeSelector); i++) {
-
+        //Create, name, and append divs
         const newContent = document.createElement("div");
         newContent.id = 'div' + i;
         const divTarget = document.getElementById("container");
         divTarget.appendChild(newContent);
         console.log("Element created.");
+        
+        //Change color on mouse-in
+        const gridSelector = document.getElementById("div" + i);
+        gridSelector.addEventListener("mouseover", function() {
+            gridSelector.style.backgroundColor = "yellow";
+        });
+        //Change color on mouse-out
+        gridSelector.addEventListener("mouseout", function() {
+            gridSelector.style.backgroundColor = "red";
+        });
     
         
     }
 
 
 };
+
